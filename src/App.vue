dc6714ea754ec3a8291fb9ff0ae3db7885c73060
Ana Sena <luiza1fofa@gmail.com>
2024-11-01 11:43:42 -0300

    componetizando a aplicação

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,9 @@
 <script setup>
 
 import { reactive } from 'vue';
+import Cabecalho from './components/Cabecalho.vue';
+import Formulario from './components/Formulario.vue';
+import ListaTarefas from './components/ListaTarefas.vue';
 
   const estado =reactive({
     filtro: 'todas',
@@ -56,44 +59,9 @@ import { reactive } from 'vue';
 
 <template>
   <div class="container">
-    <header class="p-5 mb-4 bg-light rounded-3">
-      <h1>Minhas Tarefas</h1>
-      <p> Você possui {{ getTarefasPendentes().length }} tarefas pendentes</p>
-    </header>
-
-  <form @submit.prevent="cadastraTarefa">    <!--o .prevent junto no submit evita o comportamento padrão do form que é de atualizar a página quando clicado tal como no js -->
-      <div class="row">
-        <div class="col-md-8">
-          <input :value="estado.tarefaTemp" @change="evento => estado.tarefaTemp = evento.target.value" required type="text" placeholder="Digite aqui a descrição da tarefa">
-        </div>
-        <div class="col-md-2">
-          <button type="submit" class="btn btn-primary">Cadastrar</button>
-        </div>
-        <div class="col-md-2">
-          <select @change="evento => estado.filtro = evento.target.value" class="form-control">
-            <option value="todas">Todas Tarefas</option>
-            <option value="pendentes">Pendentes</option>
-            <option value="finalizadas">Finalizadas</option>
-          </select>
-        </div>
-      </div>
-    </form>
-
-    <ul class="list-group mt-4">
-      <li class="list-group-item" v-for="tarefa in getTarefasFiltradas()">
-        <input @change="evento => tarefa.finalizada = evento.target.checked" :checked="tarefa.finalizada" :id="tarefa.titulo" type="checkbox">
-        <label :class="{ done: tarefa.finalizada }" class="ms-3" :for="tarefa.titulo">
-          {{ tarefa.titulo }}
-        </label>
-      </li>
-    </ul>
+    <Cabecalho :tarefas-pendentes="getTarefasPendentes().length" />
+    <Formulario :trocar-filtro="evento => estado.filtro = evento.target.value" :tarefa-temp="estado.tarefaTemp" :edita-tarefa-temp="evento => estado.tarefaTemp = evento.target.value" :cadastra-tarefa="cadastraTarefa"/>
+    <ListaTarefas :tarefas="getTarefasFiltradas()" />
   </div>
 
-
 </template>
-
-<style scoped>
-.done{
-  text-decoration: line-through;
-}
-</style>
---
+++ src/components/Formulario.vue
@@ -0,0 +1,23 @@
+<script setup>
+    const props = defineProps(['cadastraTarefa', 'tarefaTemp', 'trocarFiltro','editaTarefaTemp'])
+</script>
+
+<template>
+    <form @submit.prevent="props.cadastraTarefa">    <!--o .prevent junto no submit evita o comportamento padrão do form que é de atualizar a página quando clicado tal como no js -->
+    <div class="row">
+        <div class="col-md-8">
+            <input :value="props.tarefaTemp" @change="props.editaTarefaTemp" required type="text" placeholder="Digite aqui a descrição da tarefa">
+        </div>
+        <div class="col-md-2">
+            <button type="submit" class="btn btn-primary">Cadastrar</button>
+        </div>
+        <div class="col-md-2">
+            <select @change="props.trocarFiltro" class="form-control">
+                <option value="todas">Todas Tarefas</option>
+                <option value="pendentes">Pendentes</option>
+                <option value="finalizadas">Finalizadas</option>
+            </select>
+        </div>
+    </div>
+</form>
+</template>
